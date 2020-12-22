246fbe8cbd0999e379e10c3ca2310a39b868b39e
himanshupatil363 <60534916+himanshupatil363@users.noreply.github.com>
2020-12-22 09:44:12 +0530

    nakkesh

--- src/components/AboutUs/content.js
+++ src/components/AboutUs/content.js
@@ -1,9 +1,9 @@
 import React, { useDebugValue } from 'react';
 function Content() {
   return (
-    <div className="bg-green-400">
+    <div className="Content">
         <div className="flex justify-center">
-            <div className="py-4 bg-white w-full mx-20 my-4 rounded-2xl ">
+            <div className="py-4 bg-gray-300 border-2 border-gray-300 shadow-2xl w-full mx-40 my-10 rounded-2xl bg-opacity-25 text-black " style={{backdropFilter: `blur(8px)`}}>
                 <div className="flex justify-center">
                     <div className="text-3xl m-10 font-bold">Who are we</div>
                 </div>
@@ -21,11 +21,11 @@ function Content() {
                     <div><p className="text-2xl text-center text-gray-600 font-bold m-2">5</p>
                     <p>Cities</p></div>
                 </div>
-                <div className="flex justify-center">
+                <div className="flex justify-center mt-20">
                     <div className="text-3xl m-10 font-bold">Our story so far</div>
                 </div>
-                <div className="text-center px-48 my-4">
-                    <p className=" text-xl "> EasyFix was launched in November 2020. It is the largest home services platform in Asia, with presence in India, UAE, Singapore and Australia. The platform helps customers book reliable home services like beauty services, massage therapy, cleaning, plumbing, carpentry, appliance repair, painting etc. The company's vision is to empower millions of service professionals across the world to deliver services at home like never seen before. The company partners with tens of thousands of service professionals, helping them with  with training, credit, product procurement, insurance, technology etc
+                <div className="text-center px-48 my-4 mb-20">
+                    <p className=" text-xl ">19 cities in India: Ahmedabad, Bangalore, Bhopal, Bhubaneswar, Chandigarh Tricity, Chennai, Delhi NCR, Hyderabad, Indore, Jaipur, Kolkata, Lucknow, Ludhiana, Mumbai, Nagpur, Pune, Surat, Vadodara, Visakhapatnam
                     </p>
                 </div>
             </div>
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Content from '../components/AboutUs/content';
+import about from '../assets/images/about.jpg';
 function About() {
   return (
-    <div className="About">
-        <div className="flex justify-center text-5xl font-normal py-4">About EasyFix</div>
+    <div className="About" className=" bg-cover" style={{ backgroundImage: `url(${about})` }}>
+        <div className="flex justify-center text-white text-7xl font-bold py-10">About EasyFix</div>
         <Content /> 
     </div>
   );
