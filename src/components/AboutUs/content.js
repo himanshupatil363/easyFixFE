ee949ed16030e2eb5269738a1477f92ec74d4dcd
himanshupatil363 <60534916+himanshupatil363@users.noreply.github.com>
2020-12-27 19:34:35 +0530

    alt

--- src/components/AboutUs/content.js
+++ src/components/AboutUs/content.js
@@ -5,20 +5,20 @@ function Content() {
         <div className="flex justify-center my-20">
             <div>
                 <div className="flex justify-center">
-                    <div className="text-3xl m-10 font-bold">Who are we</div>
+                    <div className="text-5xl mb-20 mt-10 border-b-4 bg-primary bg-opacity-30 text-gray-800 border-primary  font-bold p-2 px-4">WHO ARE WE</div>
                 </div>
                 <div className="text-center px-48 my-4">
                     <p className=" text-xl "> EasyFix was launched in November 2020. It is the largest home services platform in Asia, with presence in India, UAE, Singapore and Australia. The platform helps customers book reliable home services like beauty services, massage therapy, cleaning, plumbing, carpentry, appliance repair, painting etc. The company's vision is to empower millions of service professionals across the world to deliver services at home like never seen before. The company partners with tens of thousands of service professionals, helping them with  with training, credit, product procurement, insurance, technology etc
                     </p>
                 </div>
-                <div className=" flex justify-between mx-60 mt-20">
+                <div className=" flex justify-between mx-60 my-32 text-black">
                     <div>
-                        <p className="text-2xl text-gray-600 text-center font-bold m-2">2500+</p>
+                        <p className="text-2xl text-center font-bold m-2">2500+</p>
                         <p>Trained Professionals</p>
                     </div>
-                    <div><p className="text-2xl text-center text-gray-600 font-bold m-2">5 Million+</p>
+                    <div><p className="text-2xl text-center font-bold m-2">5 Million+</p>
                     <p>Happy Customers</p></div>
-                    <div><p className="text-2xl text-center text-gray-600 font-bold m-2">5</p>
+                    <div><p className="text-2xl text-center font-bold m-2">5</p>
                     <p>Cities</p></div>
                 </div>
             </div>
--- src/components/AboutUs/map.js
+++ src/components/AboutUs/map.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import about from '../../assets/images/about.jpg';
+import India from '../../assets/images/map.svg';
 function About() {
   return (
-    <div className="Map" className=" pt-20 bg-cover mt-20" style={{ backgroundImage: `url(${about})` }}>
-        <div className="py-4 bg-gray-300 border-2 border-gray-300 shadow-2xl mx-40 rounded-2xl bg-opacity-25 text-black " style={{backdropFilter: `blur(8px)`}}>
-            <div className="flex justify-center mt-20">
-                <div className="text-3xl m-10 font-bold">Our story so far</div>
+    <div className="Map" className=" py-20 bg-cover mt-20" style={{ backgroundImage: `url(${about})` }}>
+        <div className="py-4 bg-gray-300 border-2 border-gray-300 shadow-2xl mx-40 rounded-2xl bg-opacity-25 text-black " style={{backdropFilter: `blur(9px)`}}>
+            <div className="flex-column text-center my-24">
+                <div className="text-5xl text-white font-bold">Our story so far</div>
+                <div className=" text-lg text-dull font-semibold mt-10">19 cities in India</div>
             </div>
-            <div className="text-center px-48 my-4 mb-20">
-                <p className=" text-xl ">19 cities in India: Ahmedabad, Bangalore, Bhopal, Bhubaneswar, Chandigarh Tricity, Chennai, Delhi NCR, Hyderabad, Indore, Jaipur, Kolkata, Lucknow, Ludhiana, Mumbai, Nagpur, Pune, Surat, Vadodara, Visakhapatnam
-                </p>
+            <div className="flex">
+                <div  className="h-screen w-11/12 object-contain">
+                  <img src={India} className="h-screen w-screen" />
+                </div>
+                <div className=" text-left w-3/12">
+                    <p className=" text-xl ">
+                      <ul className=""> <li className="p-1 text-dull">Ahmedabad</li><li className="p-1 text-dull"> Bangalore</li><li className="p-1 text-dull"> Bhopal</li><li className="p-1 text-dull"> Bhubaneswar</li><li className="p-1 text-dull"> Chandigarh Tricity</li><li className="p-1 text-dull"> Chennai</li><li className="p-1 text-dull"> Delhi NCR</li><li className="p-1 text-dull"> Hyderabad</li> <li className="p-1 text-dull">Indore</li> <li className="p-1 text-dull">Jaipur</li> <li className="p-1 text-dull">Kolkata</li> <li className="p-1 text-dull">Lucknow</li> <li className="p-1 text-dull">Ludhiana</li> <li className="p-1 text-dull">Mumbai</li><li className="p-1 text-dull"> Nagpur</li><li className="p-1 text-dull"> Pune</li><li className="p-1 text-dull"> Surat</li><li className="p-1 text-dull"> Vadodara</li> <li className="p-1 text-dull"> Visakhapatnam</li></ul>
+                    </p>
+                </div>
             </div>
         </div>
     </div>
