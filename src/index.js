29c5f89b7b326f5469ab344b54d7643165a5722b
brinda chanchad <brinda@brindas-MacBook-Pro.local>
2021-01-23 10:03:20 +0530

    hm

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // import User from './user'
-import Provider from './admin'
+import Provider from './user'
 ReactDOM.render(
 <div>
     {/* <User /> */}
