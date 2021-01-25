68e9b9f9e868fbe0110ed2f1937b2586316bbcd3
himanshupatil363 <60534916+himanshupatil363@users.noreply.github.com>
2021-01-25 10:08:17 +0530

    add

--- src/admin/components/uppane.js
+++ src/admin/components/uppane.js
@@ -4,15 +4,15 @@ import bell from '../assets/bell.svg'
 import search from '../assets/search.svg'
 const Uppane = () =>{
     return(
-        <div className="flex justify-between bg-blue-900 text-white p-4 text-lg">
-            <div>
-                <input type="text" name="" id="" placeholder="Search"/>
-                <button><img src={search} alt="" className="h-4 px-3 border border-primary p-3"/></button>
+        <div className="flex justify-between content-center items-center bg-blue-900 text-white p-4 text-lg">
+            <div className="flex">
+                <input type="text" name="" id="" placeholder="Search" className="px-4 py-1" />
+                <button className="py-1 px-2"><img src={search} alt="" className="h-5 opacity-50 hover:opacity-100 duration-1000"/></button>
             </div>
             <div className="flex justify-between p">
             <img className="h-6 mx-4" src={chat} alt=""/>
             <img className="h-6 mx-4" src={bell} alt=""/>
-            <button className="bg-white mx-4 text-blue-900 font-bold px-4 rounded-md hover:bg-blue-900 hover:text-white duration-1000">logout</button>
+            <button className="bg-white mx-4 text-blue-900 font-bold px-4 py-1 rounded-md hover:bg-blue-900 hover:text-white duration-1000">logout</button>
             </div>
         </div>
     );    
--- src/admin/pages/users.js
+++ src/admin/pages/users.js
@@ -1,9 +1,11 @@
+import React from 'react';
+import Hero from '../components/users/hero';
 import Navbar from '../components/navbar'
 const Users = () =>{
     return(
-        <div className="Users flex">
-            <Navbar />
-            admin Users
+        <div className="Users flex bg-blue-100">
+            <Navbar/>
+            <Hero />
         </div>
     );
 }
