6aeb81c42a25164b67aeb918b5538c28f6828e91
himanshupatil363 <60534916+himanshupatil363@users.noreply.github.com>
2021-02-05 20:53:01 +0530

    ll

--- src/components/categories/serinfo.js
+++ src/components/categories/serinfo.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const serinfo = (data) => {
     return (
-        <div>
-            {data.name}<br/>
-            {data.price}<br/>
-            {data.category}<br/>
-        </div>
+        <NavLink to="" className="flex flex-col">
+            <div><img className="h-80 w-80 rounded-t-xl" src={data.img} alt="not loaded" /></div>
+            <div className="mx-4 my-4">
+                <p className="mt-2 text-lg">{data.name}</p>
+                <p className="mb-2 font-thin">{data.category}</p>
+                <p className="my-2 font-bold text-md">&#8377; {data.price}</p>
+            </div>
+        </NavLink>
     )
 }
 
--- src/components/categories/service.js
+++ src/components/categories/service.js
@@ -11,31 +11,31 @@ let { id } = useParams();
           try {
             const { data } = await axios.get(`http://localhost:9990/service/cat/${id}`);
             setCatData(data);
-
         } catch (error) {
-              console.log(error)
-               
+            console.log(error)
           }
         };
         FetchCategories();
       }, [id]);
     return(
-        <div className="Cat">
+        <div className="Cat bg-blue-200">
             <div className="name">
                     <ul className="flex justify-around p-10 flex-wrap">
                     {catData.map((data, key) => {
                         return (
-                                <Serv 
+                            <li className="bg-white rounded-xl hover:shadow-2xl duration-700">
+                                <Serv
                                     key={key}
                                     name={data.name}
                                     category={data.category}
                                     price={data.price}
                                     img={data.img}
                                 />
+                                </li>
                             );
                         })
                     }
-                </ul>      
+                </ul>
             </div>
         </div>
     );
